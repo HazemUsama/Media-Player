b2b53f8a1bd6a70f225b9e13232fb90cf6baf961
HazemUsama <hazemosama681@gmail.com>
2023-05-11 19:24:01 +0200

    problem with pyinstaller

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle \
+, QSlider, QFileDialog
 from PyQt5.QtGui import QIcon, QPalette
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
 import sys
 
+#pyinstaller --onefile -windowed --icon=icon.ico main.py   
 class Window(QWidget):
 	def __init__(self):
 		super().__init__()
@@ -13,19 +15,71 @@ class Window(QWidget):
 		self.setWindowIcon(QIcon("icon.png"))
 		self.setGeometry(350, 100, 1000, 800)
 		self.setStyleSheet("background-color: #303022;")
-		
-		# p = self.palette()
-		# p.setColor(QPalette.Window, Qt.red)
-		# self.setPalette(p)
-	
+		self.create_player()
+
 	def create_player(self):
 
 		self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
 		videoWidget = QVideoWidget()
 
-		openButton = QPushButton("Open Video")
+		self.openButton = QPushButton("Open Video")
+		self.openButton.clicked.connect(self.open_file)
+		self.playButton = QPushButton("Play")
+		self.playButton.setEnabled(False)
+		self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+		self.playButton.clicked.connect(self.play_video)
+
+		hbox = QHBoxLayout()
+		hbox.setContentsMargins(0, 0, 0, 0)
+
+		self.slider = QSlider(Qt.Horizontal)
+		self.slider.setRange(0, 0)
+		self.slider.sliderMoved.connect(self.set_position)
+
+		hbox.addWidget(self.openButton)
+		hbox.addWidget(self.playButton)
+		hbox.addWidget(self.slider)
+
+		vbox = QVBoxLayout()
+		vbox.addWidget(videoWidget)
+		vbox.addLayout(hbox)
 		
+		self.mediaPlayer.setVideoOutput(videoWidget)
+
+		self.setLayout(vbox)
+
+		self.mediaPlayer.stateChanged.connect(self.mediastate_changed)
+		self.mediaPlayer.positionChanged.connect(self.position_changed)
+		self.mediaPlayer.durationChanged.connect(self.duration_changed)
+	
+	def open_file(self):
+		filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
+		
+		if filename != '':
+			self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
+			self.playButton.setEnabled(True)
+	
+	def play_video(self):
+		if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+			self.mediaPlayer.pause()
+		else:
+			self.mediaPlayer.play()
+
+	def mediastate_changed(self, state):
+		if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+			self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+		else:
+			self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+	
+	def position_changed(self, position):
+		self.slider.setValue(position)
+
+	def duration_changed(self, duration):
+		self.slider.setRange(0, duration)
+	
+	def set_position(self, position):
+		self.mediaPlayer.setPosition(position)
 
 app = QApplication(sys.argv)
 window = Window() 
