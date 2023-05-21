aeefbdbec19b6353e66671b2a915630afaa156bf
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-05-21 20:19:43 +0200

    hello zozeee

--- main.py
+++ main.py
@@ -1,83 +1,86 @@
 # import libraries
+from asyncore import loop
+from audioop import add
+from cgi import print_environ_usage
+import imp
 import os
 from tkinter import *
+import pygame
 from tkinter import filedialog
-from pygame import mixer
-
 # Create a GUI window
 root = Tk()
-root.title("Music Player")
-root.geometry("920x600+290+85")
-root.configure(background='#0f1a2b')
-root.resizable(False, False)
+root.title("Music.hub")
+root.geometry("500x400")
 
-mixer.init()
+pygame.mixer.init();
 
-# Create a function to open a file
+global pause
+pause = False
 
+song_box = Listbox(root, bg="black" , fg="blue" , width=60 , selectbackground="gray", selectforeground="black")
+song_box.pack(pady=20)
 
-def AddMusic():
-    path = filedialog.askdirectory()
-    if path:
-        os.chdir(path)
-        songs = os.listdir(path)
+def Play():
+       song = song_box.get(ACTIVE)
+       pygame.mixer.music.load(song)
+       pygame.mixer.music.play(loops = 0)
 
-        for song in songs:
-            if song.endswith(".mp3"):
-                Playlist.insert(END, song)
+def Stop():
+       pygame.mixer.music.stop()
+       song_box.selection_clear(ACTIVE)
+def add_song():
+       song = filedialog.askopenfilename(initialdir="Music/", title="choose a song" , filetypes=(("mp3 Files", ".mp3"), ))
+       song_box.insert(END, song)
 
+def add_many_song():
+       songs = filedialog.askopenfilenames(initialdir="Music/", title="choose a song" , filetypes=(("mp3 Files", ".mp3"), ))
+       for song in songs:
+              song_box.insert(END,song)
 
-def PlayMusic():
-    Music_Name = Playlist.get(ACTIVE)
-    print(Music_Name[0:-4])
-    mixer.music.load(Playlist.get(ACTIVE))
-    mixer.music.play()
+def Pause(is_paused):
+       global pause
+       pause = is_paused
+       if not pause:
+              pygame.mixer.music.pause()
+              pause = True
+       else:
+              pygame.mixer.music.unpause()
+              pause = False
 
+def next_song():
+       nxtsong = song_box.curselection()
+       song = str(nxtsong+1)
+       pygame.mixer.music.load(song)
+       pygame.mixer.music.play(loops=0)
 
-# icon
-image_icon = PhotoImage(file="~/Pictures/888881.png")
-root.iconphoto(False, image_icon)
+next_ico = PhotoImage(file="Images/next.png")
+back_ico = PhotoImage(file="Images/back.png")
+play_ico = PhotoImage(file="Images/play.png")
+pause_ico = PhotoImage(file="Images/pause.png")
+stop_ico = PhotoImage(file="Images/stop.png")
 
-Top = PhotoImage(file="~/Pictures/888881.png")
-Label(root, image=Top, bg="#0f1a2b").pack()
+control_frame = Frame(root)
+control_frame.pack()
 
-# logo
-logo = PhotoImage(file="~/Pictures/888881.png")
-Label(root, image=logo, bg="#0f1a2b", bd=0).place(x=70, y=115)
+next_button = Button(control_frame,image=next_ico, borderwidth=0, command= next_song)
+back_button = Button(control_frame,image=back_ico, borderwidth=0)
+play_button = Button(control_frame,image=play_ico, borderwidth=0 , command= Play)
+pause_butoon =Button(control_frame,image=pause_ico, borderwidth=0, command=lambda: Pause(pause) )
+stop_butoon = Button(control_frame,image=stop_ico, borderwidth=0, command= Stop)
 
-# Button
-ButtonPlay = PhotoImage(file="~/Pictures/888881.png")
-Button(root, image=ButtonPlay, bg="#0f1a2b", bd=0,
-       command=PlayMusic).place(x=100, y=400)
+next_button.grid(row=0, column=5, padx=10)
+back_button.grid(row = 0, column=1,padx=10)
+play_button.grid(row =0, column=3,padx=10)
+pause_butoon.grid(row = 0 , column=4,padx=10)
+stop_butoon.grid(row = 0, column=2,padx=10)
 
-ButtonStop = PhotoImage( file="~/Pictures/888881.png")
-Button(root, image=ButtonStop, bg="#0f1a2b", bd=0,
-       command=mixer.music.stop).place(x=30, y=500)
+my_menu = Menu(root)
+root.config(menu=my_menu)
 
-ButtonResume = PhotoImage(file="~/Pictures/888881.png")
-Button(root, image=ButtonResume, bg="#0f1a2b", bd=0,
-       command=mixer.music.unpause).place(x=115, y=500)
-
-ButtonPause = PhotoImage(file="~/Pictures/888881.png")
-Button(root, image=ButtonPause, bg="#0f1a2b", bd=0,
-       command=mixer.music.pause).place(x=200, y=500)
-
-# Label
-Menu = PhotoImage(file="~/Pictures/888881.png")
-Label(root, image=Menu, bg="#0f1a2b").pack(padx=10, pady=50, side=RIGHT)
-
-Frame_Music = Frame(root, bd=2, relief=RIDGE)
-Frame_Music.place(x=330, y=350, width=560, height=200)
-
-Button(root, text="Open Folder", width=15, height=2, font=("arial",
-       10, "bold"), fg="Black", bg="#21b3de", command=AddMusic).place(x=330, y=300)
-
-Scroll = Scrollbar(Frame_Music)
-Playlist = Listbox(Frame_Music, width=100, font=("Aloja", 10), bg="#000000",
-                   fg="white", selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
-Scroll.config(command=Playlist.yview)
-Scroll.pack(side=RIGHT, fill=Y)
-Playlist.pack(side=LEFT, fill=BOTH)
+add_song_to_menu = Menu(my_menu)
+my_menu.add_cascade(label="Add song", menu=add_song_to_menu)
+add_song_to_menu.add_command(label="Add one song" , command= add_song)
+add_song_to_menu.add_command(label="Add many songs" , command= add_many_song)
 
 # Execute Tkinter
 root.mainloop()
