749023d25944a3835f8b9d940b7dff9e3294be2b
HazemUsama <hazemosama681@gmail.com>
2023-05-22 16:18:24 +0200

    update

--- test.py
+++ test.py
@@ -1,33 +1,138 @@
-import os
+# import libraries
 from tkinter import *
 import pygame
-from PIL import Image, ImageTk
-
+from tkinter import filedialog
+import time
+from mutagen.mp3 import MP3
+import tkinter.ttk as ttk
+# Create a GUI window
 root = Tk()
 root.title("Music.hub")
-root.geometry("500x400")
+root.geometry("500x500")
 
-# Obtain the absolute path to the icon file
-home_dir = os.path.expanduser("~")
-icon_path = os.path.join(home_dir, "Desktop/Media-Player/Images/logo.png")
+pygame.mixer.init();
 
-# Set the icon for the root window
-icon = Image.open(icon_path)
-icon = icon.resize((32, 32), Image.ANTIALIAS)
+global pause
+pause = False
 
-# Set the icon photo for the root window
-icon_photo = ImageTk.PhotoImage(icon)
-root.tk.call("wm", "iconphoto", root._w, "-default", icon_photo)
+song_box = Listbox(root, bg="black" , fg="green" , width=60 , selectbackground="gray", selectforeground="black")
+song_box.pack(pady=20)
 
-pygame.mixer.init()
+def play_time():
+       current_time = pygame.mixer.music.get_pos() /1000      
+       converted_time = time.strftime("%M:%S" , time.gmtime(current_time))     
+       status_bar.after(1000, play_time)
+       current_song = song_box.curselection()
+       song = song_box.get(current_song)
+       song_mut = MP3(song)
+       global song_len
+       song_len = song_mut.info.length
+       my_slider.config(value = current_time)
+       converted_song_len = time.strftime("%M:%S" , time.gmtime(song_len))
+       status_bar.config(text=F'{converted_time}/{converted_song_len}')
 
-def play():
-	pygame.mixer.music.load("Music/Ellos.mp3")
-	pygame.mixer.music.play(loops=0)
 
-my_button = Button(root, text="Play Song", font=("Helvetica", 32), command=play)
-my_button.pack(pady=20)
 
+
+def Play():
+       song = song_box.get(ACTIVE)
+       pygame.mixer.music.load(song)
+       pygame.mixer.music.play(loops = 0)
+       play_time()
+       slider_position = int(song_len)
+       my_slider.config(to=slider_position, value=0) 
+
+def Stop():
+       pygame.mixer.music.stop()
+       song_box.selection_clear(ACTIVE)
+       status_bar.config(text='')
+
+def add_many_song():
+       songs = filedialog.askopenfilenames(initialdir="Music/", title="choose a song" , filetypes=(("mp3 Files", ".mp3"), ))
+       for song in songs:
+              song_box.insert(END,song)
+
+def remove_song():
+       song_box.delete(ANCHOR)
+       pygame.mixer.music.stop()
+
+def remove_all_songs():
+       song_box.delete(0,END)
+       pygame.mixer.music.stop()
+
+def Pause():
+       global pause 
+       if not pause:
+              pygame.mixer.music.pause()
+              pause = True
+       else:
+              pygame.mixer.music.unpause()
+              pause = False
+
+def next_song():
+       nxtsong = song_box.curselection()
+       nxtsong = nxtsong[0] + 1
+       song = song_box.get(nxtsong)
+       pygame.mixer.music.load(song)
+       pygame.mixer.music.play(loops=0)
+       song_box.select_clear(0,END)
+       song_box.activate(nxtsong)
+       song_box.select_set(nxtsong, last=None)
+
+def previous_song():
+       prevsong = song_box.curselection()
+       prevsong = prevsong[0] - 1
+       song = song_box.get(prevsong)
+       pygame.mixer.music.load(song)
+       pygame.mixer.music.play(loops=0)
+       song_box.select_clear(0,END)
+       song_box.activate(prevsong)
+       song_box.select_set(prevsong, last=None)
+
+def slide(x):
+       slider_label.config(text = int(my_slider.get()))
+
+next_ico = PhotoImage(file="Icons/next.png")
+back_ico = PhotoImage(file="Icons/previous.png")
+play_ico = PhotoImage(file="Icons/play.png")
+pause_ico = PhotoImage(file="Icons/pause.png")
+stop_ico = PhotoImage(file="Icons/stop.png")
+up_ico = PhotoImage(file="Icons/upload.png")
+
+control_frame = Frame(root)
+control_frame.pack()
+
+next_button = Button(control_frame,image=next_ico, borderwidth=0, command= next_song)
+back_button = Button(control_frame,image=back_ico, borderwidth=0, command = previous_song)
+play_button = Button(control_frame,image=play_ico, borderwidth=0 , command= Play)
+pause_butoon =Button(control_frame,image=pause_ico, borderwidth=0, command= Pause)
+stop_butoon = Button(control_frame,image=stop_ico, borderwidth=0, command= Stop)
+up_button = Button(control_frame, image= up_ico, borderwidth = 0, command = add_many_song)
+
+next_button.grid(row=0, column=5, padx=10)
+back_button.grid(row = 0, column=1,padx=10)
+play_button.grid(row =0, column=3,padx=10)
+pause_butoon.grid(row = 0 , column=4,padx=10)
+stop_butoon.grid(row = 0, column=2,padx=10)
+up_button.grid(row= 1, column = 3, pady = 10)
+
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+add_song_to_menu = Menu(my_menu)
+
+remove_song_menu = Menu(my_menu)
+my_menu.add_cascade(label="Remove song", menu=remove_song_menu)
+remove_song_menu.add_command(label="Remove one song" , command=remove_song)
+remove_song_menu.add_command(label="Remove all songs" , command=remove_all_songs)
+
+status_bar = Label(root, text='' , bd=1, relief=GROOVE , anchor=E)
+status_bar.pack(fill=X, side=BOTTOM, ipady=2)
+
+my_slider = ttk.Scale(root, from_=0 ,length=360, to=100, orient=HORIZONTAL, value= 0 , command= slide)
+my_slider.pack(pady=20)
+
+slider_label = Label(root,text="0")
+slider_label.pack(pady=10)
+# Execute Tkinter
 root.mainloop()
-
-
